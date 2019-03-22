7707191407a00969bd5c3f78d3e5245f60909f0c
Katherine Wren <kwren@outlook.com>
2019-03-22 17:39:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function logShout(string) {console.log(string.toUpperCase())}
 function logWhisper(string) {console.log(string.toLowerCase())}
 function sayHiToGrandma(string) 
 {if (string.toLowerCase()===string) {return 'I can\'t hear you!'}
-else if (string.toUpperCase()===string) {return 'YES INDEED!'}
-else if ('I love you Grandma.'===string) {return "I love you, too."}}
+if (string.toUpperCase()===string) {return 'YES INDEED!'}
+if ('I love you Grandma.'===string) {return "I love you, too."}}
