348340e9fb81e7d6f0b5b2df232c172f9551181b
Katherine Wren <kwren@outlook.com>
2019-03-22 16:32:17 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,6 @@
+function shout(string) {return string.toUpperCase()
+}
+function whisper(string) {return string.toLowerCase()}
+
+function logWhisper(string) {console.log(string.toLowerCase)}
+function logShout(string) {console.log(string.toUpperCase)}
