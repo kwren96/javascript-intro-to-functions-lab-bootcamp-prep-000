e3a2b7cbfca9a87b2d503b22b87610a5c93bb6e4
Katherine Wren <kwren@outlook.com>
2019-03-22 16:48:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function whisper(string) {return string.toLowerCase()}
 
 function logWhisper(string) {console.log(string.toLowerCase)}
 function logShout(string) {console.log(string.toUpperCase)}
-function sayHiToGrandma(whisper) {console.log ('I cant hear you')}
+function sayHiToGrandma(whisper) {console.log ('I can\'t hear you!')}
 
