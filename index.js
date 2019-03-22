c67cf79ff075fd884837b9ecf5baa15fa2cdeb82
Katherine Wren <kwren@outlook.com>
2019-03-22 17:16:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 function shout(string) {return string.toUpperCase()
 }
 function whisper(string) {return string.toLowerCase()}
-function logShout(shout) {console.log()
-function logWhisper
-function sayHiToGrandma
-    const spy = expect.spyOn(console, 'log').andCallThrough()
+function logShout(shout) {console.log('Shout')}
+function logWhisper(whisper) {console.log('Whisper')}
