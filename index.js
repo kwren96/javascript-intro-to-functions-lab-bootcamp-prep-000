099af040e34e947c2d49ad5e15c0b7d194137337
Katherine Wren <kwren@outlook.com>
2019-03-22 17:38:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ function logWhisper(string) {console.log(string.toLowerCase())}
 function sayHiToGrandma(string) 
 {if (string.toLowerCase()===string) {return 'I can\'t hear you!'}
 else if (string.toUpperCase()===string) {return 'YES INDEED!'}
-else if ('I love you Grandma.'===string) {return "I love you, too"}}
+else if ('I love you Grandma.'===string) {return "I love you, too."}}
