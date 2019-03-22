15cb938d354ec7d65c2950d63d0190e6cc297bd9
Katherine Wren <kwren@outlook.com>
2019-03-22 17:14:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function shout(string) {return string.toUpperCase()
 }
 function whisper(string) {return string.toLowerCase()}
-
+function logShout(shout) {console.log()
+function logWhisper
+function sayHiToGrandma
+    const spy = expect.spyOn(console, 'log').andCallThrough()
